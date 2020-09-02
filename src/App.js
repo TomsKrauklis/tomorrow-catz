5a6820173f0a9b6e3af47ec61a942088817d072d
Toms Krauklis <tomskrauklis@Tomss-MBP.cust.communityfibre.co.uk>
2020-09-02 18:46:59 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Homepage from './homepage.component'
+import './homepage.styles.scss'
+
+function App() {
+  return (
+    <div>
+
+<Homepage></Homepage>
+    </div>
+  );
+}
+
+export default App;
