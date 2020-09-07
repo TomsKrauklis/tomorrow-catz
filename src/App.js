ec3b0da294c10d9d78d70be90b02b23319d476f4
Toms Krauklis <tomskrauklis@Tomss-MBP.cust.communityfibre.co.uk>
2020-09-07 18:41:40 +0100

    router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import './pages/homepage/homepage.styles.scss'
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage =()=> (
+<div><h1>WHAT IS THIS</h1></div>
+
+);
+
 
 function App() {
   return (
     <div>
-
-<Homepage></Homepage>
+<switch>
+< Route exact={false} path='/' component={Homepage}/>
+</switch>
     </div>
   );
 }
